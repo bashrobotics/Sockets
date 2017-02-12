291130f2a5bdc0b363b0775f3b61266bf00bb03c
Quentin Leon <qwolf1999@gmail.com>
2017-02-11 20:48:18 -0800

    sockets now dynamically get the Jetson's ip and connect to it

--- src/org/usfirst/frc/team972/robot/Robot.java
+++ src/org/usfirst/frc/team972/robot/Robot.java
@@ -3,6 +3,7 @@ package org.usfirst.frc.team972.robot;
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.InputStreamReader;
+import java.net.InetAddress;
 import java.net.Socket;
 
 import edu.wpi.first.wpilibj.*;
@@ -10,27 +11,45 @@ import edu.wpi.first.wpilibj.*;
 public class Robot extends IterativeRobot {
 	public void teleopInit() {
 		try {
-			testConnect();
+			String ip = getJetsonIP();
+			connect(ip);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 	
-	public static void testConnect() throws Exception{
-		String ip = "10.9.72.37";
+	public void connect(String ip) throws Exception{
 		int port = 9720;
+		String confirmationToken = "972";
+		System.out.println("Connecting to " + ip + ":" + port + " with confirmation token:'" + confirmationToken + "'");
 		
 		Socket clientSocket = new Socket(ip, port);
 		DataOutputStream toServer = new DataOutputStream(clientSocket.getOutputStream());
 		BufferedReader fromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-		
-		String confirmationToken = "972";
+	
 		String received;
-		
 		toServer.writeBytes(confirmationToken + '\n');
 		received = fromServer.readLine();
 		System.out.println(received);
 		clientSocket.close();
+	}
+	
+	public String getJetsonIP() throws Exception{ //attempt at dynamically getting the Jetson's ip
+		String localIP = InetAddress.getLocalHost().getHostAddress();
+		System.out.println("RoboRio is on IP address " + localIP);
+		String subnet = localIP.substring(0,localIP.length() - 3);
+		System.out.println("We need to search subnet " + subnet);
+		int timeout = 5;
+		for(int i = 1; i < 255; i++){
+			String ip = subnet + "." + i;
+			if(InetAddress.getByName(ip).isReachable(timeout)){
+				System.out.println(ip + " is reachable");
+				if(!ip.equals(localIP) && i != 1 && i != 255){
+					return ip;
+				}
+			}
+		}
+		return null;
 	}
 }
 
